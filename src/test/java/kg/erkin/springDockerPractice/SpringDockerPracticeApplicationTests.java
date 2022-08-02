1c360cde2263271b52abd54a015823a052752d5a
Erkin <erkin1399@gmail.com>
2022-08-02 18:47:48 +0600

    Comment tests

--- src/test/java/kg/erkin/springDockerPractice/SpringDockerPracticeApplicationTests.java
+++ src/test/java/kg/erkin/springDockerPractice/SpringDockerPracticeApplicationTests.java
@@ -1,13 +1,13 @@
-package kg.erkin.springDockerPractice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringDockerPracticeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package kg.erkin.springDockerPractice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringDockerPracticeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
